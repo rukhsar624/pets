b17cea0ee51bb21a1876a5125d00be72b7abe6f0
rukhsar624 <rukhsar56@gmail.com>
2023-03-12 04:18:53 -0700

    add saturday code

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,6 +45,7 @@ window.addEventListener('keydown', function (event) {
 var swiper = new Swiper(".mySwiper", {
 	slidesPerView: 3,
 	spaceBetween: 30,
+	loop:true,
 	navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
